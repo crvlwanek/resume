a2f8ee975b33eccd025c29828c065631f832ba33
ChrisVL-W <chris.vanlanenwanek@gmail.com>
2021-07-02 23:11:13 -0500

    Updates

--- component---src-pages-404-js-e3b7d0283d697ae6d052.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{9616:function(t,e,n){"use strict";n.r(e);var u=n(7294),a=n(3542),l=n(3751);e.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-e3b7d0283d697ae6d052.js.map
